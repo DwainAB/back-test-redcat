b28d58a37d7fdce6e88dd3cfd0574d6bd275b2c8
Dwain <dwaincontact@gmail.com>
2023-07-12 22:00:34 +0200

    modif

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-var_dump('test');
-?>
+// Votre logique de traitement de la requête ici
+
+// Exemple : Renvoyer une réponse JSON
+header('Content-Type: application/json');
+
+$response = [
+  'message' => 'Bienvenue sur mon API PHP !',
+  'timestamp' => time()
+];
+
+echo json_encode($response);
